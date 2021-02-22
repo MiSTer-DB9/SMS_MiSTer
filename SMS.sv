0cd0ccd5740a1be62936fb6c304c83631849e256
Miguel-T80c <the80scomputers@gmail.com>
2021-02-22 23:39:12 +0100

    Fix User IO selector problem.

--- SMS.sv
+++ SMS.sv
@@ -178,7 +178,7 @@ module emu
 assign ADC_BUS  = 'Z;
 
 wire         CLK_JOY = CLK_50M;         //Assign clock between 40-50Mhz
-wire   [2:0] JOY_FLAG  = {status[30],status[31],status[29]}; //Assign 3 bits of status (31:29) o (63:61)
+wire   [2:0] JOY_FLAG  = {status[62],status[63],status[61]}; //Assign 3 bits of status (31:29) o (63:61)
 wire         JOY_CLK, JOY_LOAD, JOY_SPLIT, JOY_MDSEL;
 wire   [5:0] JOY_MDIN  = JOY_FLAG[2] ? {USER_IN[6],USER_IN[3],USER_IN[5],USER_IN[7],USER_IN[1],USER_IN[2]} : '1;
 wire         JOY_DATA  = JOY_FLAG[1] ? USER_IN[5] : '1;
@@ -259,8 +259,8 @@ parameter CONF_STR = {
 
 	"P2,Input;",
 	"P2-;",
-	"P2OUV,UserIO Joystick,Off,DB9MD,DB15 ;",
-	"P2OT,UserIO Players, 1 Player,2 Players;",
+	"P2oUV,UserIO Joystick,Off,DB9MD,DB15 ;",
+	"P2oT,UserIO Players, 1 Player,2 Players;",
 	"P2-;",
 	"P2O1,Swap joysticks,No,Yes;",
 	"P2OE,Multitap,Disabled,Port1;",
